db9548b256fbce4a2632c67252b6233bac9a1eb5
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-06-22 17:55:55 -0400

    feat: create function scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,7 +29,11 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_url = selector.css(".next::attr(href)").get()
+    if not next_page_url:
+        None
+    return next_page_url
 
 
 # Requisito 4
