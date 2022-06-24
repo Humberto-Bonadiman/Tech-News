17294d565899036554690f8f766aed3882b893d5
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-06-23 21:54:25 -0400

    feat: requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,7 +38,30 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("[rel='canonical']::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get()
+    timestamp = selector.css(".post-meta .meta-date::text").get()
+    writer = selector.css(".meta-author a::text").get()
+    comments_count = selector.css(".comment-content::text").getall()
+    list_summary = selector.css(
+        "div.entry-content p:nth-child(2) *::text"
+    ).getall()
+    summary = ""
+    for text in list_summary:
+        summary += text
+    tags = selector.css('[rel="tag"]::text').getall()
+    category = selector.css(".meta-category .label::text").get()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": len(comments_count),
+        "summary": summary,
+        "tags": tags,
+        "category": category
+    }
 
 
 # Requisito 5
